2d1c765c256bf3003d1a08c8e6018bf889eaeedc
AJ Danelz <@work>
2018-10-26 16:00:04 -0400

    add example

---
+++ src/main/java/org/mapstruct/proto/mapper/UserMapper.java
@@ -0,0 +1,53 @@
+/**
+ * Copyright 2012-2017 Gunnar Morling (http://www.gunnarmorling.de/)
+ * and/or other contributors as indicated by the @authors tag. See the
+ * copyright.txt file in the distribution for a full listing of all
+ * contributors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.mapstruct.proto.mapper;
+
+import org.mapstruct.CollectionMappingStrategy;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingConstants;
+import org.mapstruct.NullValueCheckStrategy;
+import org.mapstruct.ValueMapping;
+import org.mapstruct.factory.Mappers;
+import org.mapstruct.proto.UserProtos;
+import org.mapstruct.proto.protobuf.Permission;
+import org.mapstruct.proto.protobuf.User;
+
+/**
+ * @author Thomas Kratz
+ */
+@Mapper(collectionMappingStrategy = CollectionMappingStrategy.ADDER_PREFERRED,
+        nullValueCheckStrategy = NullValueCheckStrategy.ALWAYS)
+public interface UserMapper {
+
+    UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
+
+
+    @Mapping(source = "permissions", target = "permissionsList")
+    UserProtos.UserDTO map(User user);
+
+    @Mapping(source = "permissionsList", target = "permissions")
+    User map(UserProtos.UserDTO userDTO);
+
+    @ValueMapping(source = "UNRECOGNIZED", target = MappingConstants.NULL)
+    Permission map(UserProtos.PermissionDTO permissionDTO);
+
+    UserProtos.PermissionDTO map(Permission perm);
+
+}
---
+++ src/main/java/org/mapstruct/proto/protobuf/Permission.java
@@ -0,0 +1,11 @@
+package org.mapstruct.proto.protobuf;
+
+/**
+ * @author Thomas Kratz
+ */
+public enum Permission {
+
+    ADMIN,
+    USER,
+    NONE
+}
